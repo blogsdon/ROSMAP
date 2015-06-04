91f26e314442a61d458d888c9e094d91b62eb9aa
Ben Logsdon <aeberhart@gmail.com>
2015-06-04 10:23:40 -0700

    script to push sparrow nci nets

---
+++ pushSparrowNets.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+/shared/metanetworkSynapse/pushNet.sh -a "syn4261317" -b "/shared/ROSMAP/code.txt" -c "/shared/ROSMAP/syn.txt" -dev -r "none" -s "HomoSapiens" -t "Control" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
