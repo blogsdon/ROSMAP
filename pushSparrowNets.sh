631085ecfe4cd241ea597703c0970e1ea54ae440
Ben Logsdon <aeberhart@gmail.com>
2015-06-04 13:00:18 -0700

    updated network push scritp

--- pushSparrowNets.sh
+++ pushSparrowNets.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
+#$ -S /bin/bash
+#$ -V
+#$ -cwd
+#$ -N pushingnetworks
+#$ -e error.txt
+#$ -o out.txt
+
+
 /shared/metanetworkSynapse/pushNet.sh -a "syn4261317" -b "/shared/ROSMAP/code.txt" -c "/shared/ROSMAP/syn.txt" -dev -r "none" -s "HomoSapiens" -t "Control" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
