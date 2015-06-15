dd4c5aeaf7212b9f51ce15f967cfeecf571882db
Ben Logsdon <aeberhart@gmail.com>
2015-06-15 11:32:19 -0700

    run rosmap nci

--- runROSMAPnci.sh
+++ runROSMAPnci.sh
@@ -1,5 +1,22 @@
 #!/bin/bash
-#simulate some data
-qsub -v dataFile="../ROSMAP/NCIrna.csv",pathv="/shared/metanetworkSynapse/",sparrowZ=1,sparrow2Z=1,numberCore=160,outputpath="/shared/ROSMAP/NCI/" -pe orte 160 buildNet.sh
-#qsub -v dataFile="../ROSMAP/NCIrna.csv",pathv="/shared/metanetworkSynapse/ARACNE/",correlation=1,correlationBonferroni=1,correlationFDR=1,wgcna=1,numberCore=160,outputpath="/shared/ROSMAP/NCI" -pe orte 160 buildNet.sh
-#./pushNet.sh -a "syn4229809" -b "../ROSMAP/code.txt" -c "../ROSMAP/syn.txt" -defghijklmnopqv -r "none" -s "HomoZapiens" -t "bonitis" -u "blood" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+
+rm codeNCI.txt
+rm syn.txt
+
+#./grabCMCdata.sh
+
+echo "syn4259377" >> syn.txt
+echo "syn4259379" >> syn.txt
+
+echo "https://github.com/blogsdon/ROSMAP/blob/master/runROSMAPnci.sh" >> codeNCI.txt
+echo "https://github.com/blogsdon/ROSMAP/blob/master/getROSMAPrnaseq.R" >> codeNCI.txt
+
+cd ../metanetworkSynapse/
+qsub -v dataFile="../ROSMAP/NCIrna.csv",pathv="/shared/metanetworkSynapse/",sparrow1=1,sparrow2=1,lassoCV1se=1,lassoCV1min=1,lassoAIC=1,lassoBIC=1,ridgeCV1se=1,ridgeCV1min=1,ridgeAIC=1,ridgeBIC=1,genie3=1,tigress=1,numberCore=150,outputpath="/shared/ROSMAP/NCI/" -pe orte 150 buildNet.sh
+qsub -v dataFile="../ROSMAP/NCIrna.csv",pathv="/shared/metanetworkSynapse/",aracne=1,aracneFull=1,correlation=1,correlationBonferroni=1,correlationFDR=1,wgcna=1,numberCore=1,numberCore=1,outputpath="/shared/ROSMAP/NCI" -pe orte 1 buildNet.sh
+
+
+#qsub -v dataFile="../CMC/controlData.csv",pathv="/shared/metanetworkSynapse/ARACNE/",aracne=1,correlation=1,numberCore=8 -pe orte 8 buildNet.sh
+#./pushNet.sh -a "syn3526285" -b "../CMC/code.txt" -c "../CMC/syn.txt" -defghijklmnopq -r "SVA" -s "HomoSapiens" -t "Schizophrenia" -u "DorsolateralPrefrontalCortex"
+#./cleanNet.sh
+
