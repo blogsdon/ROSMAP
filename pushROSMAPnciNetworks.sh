912812ad06a5c9339bc42e6aa9f15da07d93122d
Ben Logsdon <aeberhart@gmail.com>
2015-06-23 07:24:27 -0700

    push networks to synapse

---
+++ pushROSMAPnciNetworks.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+#$ -S /bin/bash
+#$ -V
+#$ -cwd
+#$ -N pushingcmcnetworks
+#$ -e error.txt
+#$ -o out.txt
+
+
+#nci
+/shared/metanetworkSynapse/pushNet.sh -a "syn4545016" -b "/shared/ROSMAP/codeNCI.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "NCI" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+
+#mci
+#/shared/metanetworkSynapse/pushNet.sh -a "syn4545013" -b "/shared/ROSMAP/codeMCI.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "MCI" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+
+#ad
+#/shared/metanetworkSynapse/pushNet.sh -a "syn4545005" -b "/shared/ROSMAP/codeAD.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "AD" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
