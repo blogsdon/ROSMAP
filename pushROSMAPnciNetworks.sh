b9bb30a9612b583c4a465023ac14b0050de559a2
Ben Logsdon <baelogsdon@gmail.com>
2015-07-06 15:31:42 -0700

    updated push scripts

--- pushROSMAPnciNetworks.sh
+++ pushROSMAPnciNetworks.sh
@@ -3,16 +3,12 @@
 #$ -S /bin/bash
 #$ -V
 #$ -cwd
-#$ -N pushingcmcnetworks
-#$ -e error.txt
-#$ -o out.txt
+#$ -N nci_rosmap_networks
+#$ -e error_nci_networks.txt
+#$ -o out_nci_networks.txt
+#$ -pe orte 16
 
 
 #nci
-/shared/metanetworkSynapse/pushNet.sh -a "syn4545016" -b "/shared/ROSMAP/codeNCI.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "NCI" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
-
-#mci
-#/shared/metanetworkSynapse/pushNet.sh -a "syn4545013" -b "/shared/ROSMAP/codeMCI.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "MCI" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
-
-#ad
-#/shared/metanetworkSynapse/pushNet.sh -a "syn4545005" -b "/shared/ROSMAP/codeAD.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "AD" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+#/shared/metanetworkSynapse/pushNet.sh -a "syn4545016" -b "/shared/ROSMAP/codeNCI.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopqv -r "None" -s "HomoSapiens" -t "NCI" -u "DLPFC" -x "/shared/metanetworkSynapse/pushNetworkSynapse.R"
+/shared/metanetworkSynapse/pushNet.sh -a "syn4545016" -b "/shared/ROSMAP/codeNCI.txt" -c "/shared/ROSMAP/syn.txt" -defghijklmnopq -r "None" -s "HomoSapiens" -t "NCI" -u "DLPFC" -x "/shared/metanetworkSynapse/pushSparseNetworkSynapse.R"
