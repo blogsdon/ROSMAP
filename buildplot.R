b0e25db1bfece161a3ffc4c1cb3c7d95a4099002
Ben Logsdon <baelogsdon@gmail.com>
2015-08-21 07:56:34 -0700

    make ppi plots

---
+++ buildplot.R
@@ -0,0 +1,44 @@
+#script to build ppi  plots
+require(synapseClient)
+synapseLogin()
+
+a <- synGet('syn4896557')
+
+library(data.table)
+require(dplyr)
+
+   
+b <- fread(a@filePath) %>% data.frame
+
+b$method[b$method=='sparsityconsensus'] <- 'sparsity\nconsensus'
+b$tdrVec <- (b$tdrVec)*100
+b$powerVec <- (b$powerVec)*100
+
+
+require(ggplot2)
+
+t <- ggplot(b, aes(method,tdrVec)) + geom_bar(stat='identity')
+t + facet_wrap(~ sparsityMethod)
+
+
+methodMeans <- aggregate(tdrVec ~ method, data = b, FUN = mean)
+methodMeans <- arrange(methodMeans, tdrVec)
+sparsityMethodMeans <- aggregate(tdrVec ~ sparsityMethod, data = b, FUN = mean)
+sparsityMethodMeans <- arrange(sparsityMethodMeans, tdrVec)
+
+methodMeans2 <- aggregate(powerVec ~ method, data=b, FUN = mean)
+methodMeans2 <- arrange(methodMeans2,powerVec)
+sparsityMethodMeans2 <- aggregate(powerVec ~ sparsityMethod, data = b, FUN = mean)
+sparsityMethodMeans2 <- arrange(sparsityMethodMeans2, powerVec)
+
+b$method2 <- factor(b$method, levels = methodMeans$method)
+b$method3 <- factor(b$method, levels = methodMeans2$method)
+
+
+b$sparsityMethod2 <- factor(b$sparsityMethod, levels = sparsityMethodMeans$sparsityMethod)
+b$sparsityMethod3 <- factor(b$sparsityMethod2, levels = sparsityMethodMeans2$sparsityMethod)
+
+ggplot(b) + theme_bw() + geom_bar(aes(x = method2, y = tdrVec, fill = sparsityMethod2), position = "dodge", stat = "identity") + xlab("Method") + ylab("True Discovery Rate") + scale_fill_discrete(name = "Sparsity Method") + ggtitle("True Discovery Rate")
+
+
+ ggplot(b) + theme_bw() + geom_bar(aes(x = method3, y = powerVec, fill = sparsityMethod3), position = "dodge", stat = "identity") + xlab("Method") + ylab("Power") + scale_fill_discrete(name = "Sparsity Method") + ggtitle("Power")
