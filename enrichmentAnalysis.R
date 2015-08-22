cfb9d52eef482169e280e2433ac8035b20e53b55
Ben Logsdon <baelogsdon@gmail.com>
2015-08-22 10:06:46 -0700

    updated enrichment analysis

--- enrichmentAnalysis.R
+++ enrichmentAnalysis.R
@@ -65,28 +65,55 @@ networkObj <- synGet(foo$file.id[1])
 modulesObj <- synGet(foo$file.id[2])
 enrichmentObj <- synGet(foo$file.id[3])
 nodeProperities <- synGet(foo$file.id[4])
+
+enrichments <- fread(enrichmentObj@filePath) %>% data.frame
+enrichments <- arrange(enrichments,pval)
+astrocyte <- filter(enrichments,GeneSetName=='Zhang:Astrocyte')
+microglia <- filter(enrichments,GeneSetName=='Zhang:Microglia')
+neuron <- filter(enrichments,GeneSetName=='Zhang:Neuron')
+endothelial <- filter(enrichments,GeneSetName=='Zhang:Endothelial')
+newoligos <- filter(enrichments,GeneSetName=='Zhang:NewOligos')
+opc <- filter(enrichments,GeneSetName=='Zhang:OPC')
+myelinoligos <- filter(enrichments,GeneSetName=='Zhang:MyelinOligos')
+myelinoligos[1:5,]
+
 load(networkObj@filePath)
 mods <- fread(modulesObj@filePath) %>% data.frame
 tab <- table(mods$moduleNumber)
 mods <- filter(mods,mods$moduleNumber%in%1:37)
-brownAdj <- sparseNetwork[mods$modulelabels=='brown',mods$modulelabels=='brown'] %>% as.matrix
+
+brownAdj <- sparseNetwork[mods$GeneIDs[mods$modulelabels=='brown'],mods$GeneIDs[mods$modulelabels=='brown']] %>% as.matrix
+allAdj <- sparseNetwork[mods$GeneIDs,mods$GeneIDs] %>% as.matrix
 
 library(metanetwork)
-edgeList <- rankedEdgeList(brownAdj,symmetric=T)
-
+brownedgeList <- rankedEdgeList(brownAdj,symmetric=T)
+alledgeList <- rankedEdgeList(allAdj,symmetric=T)
 
 exprFile <- synGet('syn4259377')
 require(data.table);
 expr <- fread(exprFile@filePath,header=T)
 expr <- data.frame(expr)
 
-edgeList$var1 <- expr$hgnc_symbol[mods$modulelabels=='brown'][edgeList$var1]
-edgeList$var2 <- expr$hgnc_symbol[mods$modulelabels=='brown'][edgeList$var2]
-edgeList <- filter(edgeList,!is.na(var1))
-edgeList <- filter(edgeList,!is.na(var2))
-edgeList <- filter(edgeList,var1!='')
-edgeList <- filter(edgeList,var2!='')
-write.csv(edgeList,file='~/Desktop/brown.csv',quote=F,row.names=F)
+hgnc <- expr$hgnc_symbol
+names(hgnc) <- expr$ensembl_gene_id
+
+alledgeList$var1 <- hgnc[mods$GeneIDs][alledgeList$var1]
+alledgeList$var2 <- hgnc[mods$GeneIDs][alledgeList$var2]
+
+alledgeList <- filter(alledgeList,!is.na(var1))
+alledgeList <- filter(alledgeList,!is.na(var2))
+alledgeList <- filter(alledgeList,var1!='')
+alledgeList <- filter(alledgeList,var2!='')
+write.csv(alledgeList,file='~/Desktop/allAlzROSMAP.csv',quote=F,row.names=F)
+mod2 <- select(mods,GeneIDs,modulelabels)
+mod2$GeneIDs <- hgnc[mod2$GeneIDs]
+write.csv(mod2,file='~/Desktop/allAlzMods.sv',quote=F,row.names=F)
+
+
+cytoObj <- File('~/Projects/ROSMAP/rosmap.cys',parentId='syn4545005',name='sparsityconsensus Sparrow2Bonferroni Cytoscape Session')
+cytoObj <- synStore(cytoObj)
+
+
 bar <- fread('~/Desktop/brownProp.csv') %>%data.frame(stringsAsFactors=F)
 bar <- select(bar,name,Degree,AverageShortestPathLength,BetweennessCentrality,ClosenessCentrality,ClusteringCoefficient)
 bar$Degree <- as.numeric(bar$Degree)
