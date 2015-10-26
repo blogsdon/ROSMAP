02cccb6c12dcb49068f32cb2911005e2120d25b3
Benjamin Logsdon <aeberhart@gmail.com>
2015-10-26 07:54:27 -0700

    better submission command + methods to combine

--- generateBraak12Consensus.sh
+++ generateBraak12Consensus.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-qsub -v method="methods.txt",s3path="s3://metanetworks/ROSMAP/BRAAK12/"
+qsub -v method="methods.txt",s3path="s3://metanetworks/ROSMAP/BRAAK12/" -pe orte 16 -S /bin/bash -V -cwd -N braak12cons -e /shared/ROSMAP/braak12error.txt -o /shared/ROSMAP/braak12out.txt /shared/metanetworkSynapse/pushConsensus.sh
