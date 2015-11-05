2d6b27094d5c0eaf0df3835f3765c29d52283274
Benjamin Logsdon <aeberhart@gmail.com>
2015-11-04 16:38:45 -0800

    fix bugs

--- pushCombinedRankConsensus.sh
+++ pushCombinedRankConsensus.sh
@@ -5,4 +5,4 @@ echo "syn4259377" >> synFile.txt
 
 aws s3 cp s3://metanetworks/ROSMAP/result_rankConsensus.rda ./
 aws s3 cp s3://metanetworks/ROSMAP/sparsity.csv ./
-../metanetworkSynapse/pushNet.sh -a "syn4545002" -b "/shared/ROSMAP/codeFile.txt" -c "/shared/ROSMAP/synFile.txt" -z -r "None" -s "HomoSapiens" -t "NCI_MCI_AD" -u "DLPFC" -x "/shared/metanetworkSynapse/pushSparseNetworkSynapse.R"
+../metanetworkSynapse/pushNet.sh -a "syn4545002" -b "codeFile.txt" -c "synFile.txt" -z -r "None" -s "HomoSapiens" -t "NCI_MCI_AD" -u "DLPFC" -x "../metanetworkSynapse/pushSparseNetworkSynapse.R"
