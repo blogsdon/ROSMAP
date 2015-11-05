572675ea01861f71de77138a0d19da27b9957f84
Benjamin Logsdon <aeberhart@gmail.com>
2015-11-04 16:36:46 -0800

    new script

---
+++ pushCombinedRankConsensus.sh
@@ -0,0 +1,8 @@
+echo "https://github.com/blogsdon/ROSMAP/blob/master/combinerank.sh" > codeFile.txt
+
+echo "syn4259379" > synFile.txt
+echo "syn4259377" >> synFile.txt
+
+aws s3 cp s3://metanetworks/ROSMAP/result_rankConsensus.rda ./
+aws s3 cp s3://metanetworks/ROSMAP/sparsity.csv ./
+../metanetworkSynapse/pushNet.sh -a "syn4545002" -b "/shared/ROSMAP/codeFile.txt" -c "/shared/ROSMAP/synFile.txt" -z -r "None" -s "HomoSapiens" -t "NCI_MCI_AD" -u "DLPFC" -x "/shared/metanetworkSynapse/pushSparseNetworkSynapse.R"
