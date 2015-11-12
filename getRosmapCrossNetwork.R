d2457c03a6c948d4e4415a46a40aa887239cebe2
Benjamin Logsdon <aeberhart@gmail.com>
2015-11-11 16:51:10 -0800

    scale and winsorize

--- getRosmapCrossNetwork.R
+++ getRosmapCrossNetwork.R
@@ -16,4 +16,15 @@ foo <- synGet('syn4922923')
 sampleId <- scan(foo@filePath,what='character')
 rownames(bar) <- sampleId
 
+winsorize <- function(x,per=.99){
+up <- quantile(x,per)
+low <- quantile(x,1-per)
+x[x>=up] <- up
+x[x<=low] <- low
+return(x)
+}
+bar <- apply(bar,2,winsorize)
+bar <- scale(bar)
+
+
 write.csv(bar,file='rosmapRNAseq.csv',quote=F)
